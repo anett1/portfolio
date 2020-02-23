a68c48f4622cf36fa5e1090e7d05512fe967ef9e
Aneta Jagla <anett@poczta.fm>
2020-02-23 21:26:03 +0100

    Updates

---
+++ precache-manifest.06d7d3128c63f4ecf6dbf55abfdab310.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae66f65b85ea236fb376ba2078c74459",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "18221cdc968ad7b42dd1",
+    "url": "/portfolio/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "fe7e481017988c6decc8",
+    "url": "/portfolio/static/css/main.517931bb.chunk.css"
+  },
+  {
+    "revision": "18221cdc968ad7b42dd1",
+    "url": "/portfolio/static/js/2.60f1a4d6.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/portfolio/static/js/2.60f1a4d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7e481017988c6decc8",
+    "url": "/portfolio/static/js/main.dd276762.chunk.js"
+  },
+  {
+    "revision": "debb28456858994a1826",
+    "url": "/portfolio/static/js/runtime-main.3e206433.js"
+  },
+  {
+    "revision": "0626c8bcfbc35209788963c2f2b2e307",
+    "url": "/portfolio/static/media/1.0626c8bc.png"
+  },
+  {
+    "revision": "c8cf12fec3e11176b389df94cc25bb02",
+    "url": "/portfolio/static/media/1.c8cf12fe.png"
+  },
+  {
+    "revision": "ca6cb661f4beeeb7f0d29a647986e7ec",
+    "url": "/portfolio/static/media/2.ca6cb661.png"
+  },
+  {
+    "revision": "d8c76b741e0470e780b9db6537b12e10",
+    "url": "/portfolio/static/media/2.d8c76b74.png"
+  },
+  {
+    "revision": "0eaab14e2d954ff0eec76dfc45a79250",
+    "url": "/portfolio/static/media/3.0eaab14e.png"
+  },
+  {
+    "revision": "f11b2a9861e07e1f262c2dcec793e310",
+    "url": "/portfolio/static/media/3.f11b2a98.png"
+  },
+  {
+    "revision": "7b662e5304c7d187adfc1b7c99f9bb1f",
+    "url": "/portfolio/static/media/4.7b662e53.png"
+  },
+  {
+    "revision": "ebc269dfe37ce48feaadf70c781bcc20",
+    "url": "/portfolio/static/media/4.ebc269df.png"
+  },
+  {
+    "revision": "4825f6bbe096d81a5b6bc041d5e64fae",
+    "url": "/portfolio/static/media/5.4825f6bb.png"
+  },
+  {
+    "revision": "9581352da3da39070a08bdc54f6c74eb",
+    "url": "/portfolio/static/media/5.9581352d.png"
+  },
+  {
+    "revision": "3da63f9eb8a14f120ab0d93d597e80ec",
+    "url": "/portfolio/static/media/6.3da63f9e.png"
+  },
+  {
+    "revision": "1c1eaa4a634fb29a5c0cf8b3fa3b5682",
+    "url": "/portfolio/static/media/7.1c1eaa4a.png"
+  },
+  {
+    "revision": "781ea64c2e89da70799aa03cf6001ae2",
+    "url": "/portfolio/static/media/8.781ea64c.png"
+  },
+  {
+    "revision": "f4ff3989c34a44bbbcf15bcfa5a7226f",
+    "url": "/portfolio/static/media/9.f4ff3989.png"
+  },
+  {
+    "revision": "d8fe23e7c5e1ee8a99b20d4c94314947",
+    "url": "/portfolio/static/media/aneta2.d8fe23e7.jpg"
+  },
+  {
+    "revision": "65c6b4bfc592e30cc85b89fe7bdec56e",
+    "url": "/portfolio/static/media/cv.65c6b4bf.pdf"
+  }
+]);
